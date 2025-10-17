88960ff1749645b79203abdae74572ff6a3df789
be0825 <eb082025@gmail.com>
2025-10-18 00:01:33 +0200

    first draft

---
+++ script.js
@@ -0,0 +1,13 @@
+//const myCanvas = document.getElementById("myCanvas");
+myCanvas.textContent = "testintstetklejklejlkejklj";
+myCanvas.heigth = "50px";
+myCanvas.style.background = "blue";
+myDiv.height = "50px";
+myDiv.width = "50px";
+myDiv.style.backgroundColor = "red";
+myDiv.style.color = "gold";
+
+function focus() {
+  const focusThis = document.getElementById("focus");
+  focusThis.focus();
+}
