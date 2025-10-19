1b5a1b5fdc51144175d9b039b0a8bb068f1dfdea
be0825 <eb082025@gmail.com>
2025-10-19 18:16:17 +0200

    finished

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-//const myCanvas = document.getElementById("myCanvas");
-myCanvas.textContent = "testintstetklejklejlkejklj";
-myCanvas.heigth = "50px";
-myCanvas.style.background = "blue";
-myDiv.height = "50px";
-myDiv.width = "50px";
-myDiv.style.backgroundColor = "red";
-myDiv.style.color = "gold";
-
-function focus() {
-  const focusThis = document.getElementById("focus");
-  focusThis.focus();
-}
+// don't need JavaScript
